73826cc530d486318d692c0f155e5eb3195312b0
Rostyk <pas.ros.bor@gmail.com>
2014-03-27 14:32:35 +0200

    1.2

--- script.js
+++ script.js
@@ -11,55 +11,22 @@
         */      
     function Timer(obj)
     {
-       
-        /**
-        * @property {function} action. Function for timer.
-        */
         this.action = obj.action;
-
-        /**
-        * @property {number} frameCount. Number of frames to be performed.
-        */
-        this.iteration = obj.iteration;
-
-        /**
-        * @property {number} interval. Interval for color change.
-        */
+        this.countIteration = obj.countIteration;
         this.interval = obj.interval;        
-
         this.currentIteration = 0;
-        /**
-        * @property {nimber} intervalId. intervalId for function stop().
-        */
         this.intervalId = 0;
 
-        /**
-        * This function starts the timer.
-        */
         this.run = function()
         {     
-            this.intervalIid = setInterval( function(){
-                            if(this.iteration>this.currentIteration)
-                                {
-                                    this.action; 
-                                    this.currentIteration++;
-                                }
-                            else
-                                {
-                                    this.stop();
-                                }
-                        }, this.interval);
+            this.intervalIid = setInterval( this.action.myBind(AnimateColor), this.interval)
         }
 
-        /**
-        * This function stops the timer.
-        */
         this.stop = function()
         {
             clearInterval(this.intervalId);
             this.intervalId = 0;
         }
-        
     }
 
     /**
@@ -72,76 +39,42 @@
         */   
     function AnimateColor(obj)
     {
-
-        /**
-        * @property {html element} element. html element which will be animation color.
-        */  
         this.element=obj.element;
-
-        /**
-        * @property {array} startColor. color with which to start the animation.
-        */ 
         this.startColor = obj.startColor;
-
-        /**
-        * @property {array} endColor. finish color.
-        */ 
         this.endColor = obj.endColor;
-
-
         this.time = obj.time;
-       
-
-        /**
-        * @property {array} stepChengOfColor. step that will change the color
-        */ 
         this.stepChengOfColor=[];
-
-        /**
-        * @property {array} arrayColorToChanje. color for display.
-        */ 
-        this.arrayColorToChanje = [];
-
-        /**
-        * @property {number} currentFrame. Current frame.
-        */       
+        this.arrayColorToChange = obj.startColor.map(function(item){return item;});     
         this.currentFrame=0;
+        this.countIteration = 0;
 
-        this.iteration = 0;
-         /**
-        * Counts the number of frames.
-        * @returns {number} count of animation.
-        */
         this.getIteration = function()
         {
-            this.iteration = Math.floor(this.time / 30);
-            return this.iteration;
+            this.countIteration = Math.floor(this.time / 30);
+            return this.countIteration;
         }
        
 
        this.setChanjeStepOfColor = function()
         {
-            var inter = this.getIteration();
-            this.stepChengOfColor[0] = ((this.startColor[0] - this.endColor[0]) / inter);
-            this.stepChengOfColor[1] = ((this.startColor[1] - this.endColor[1]) / inter);
-            this.stepChengOfColor[2] = ((this.startColor[2] - this.endColor[2]) / inter);
+            var iter = this.getIteration();
+            this.stepChengOfColor[0] = ((this.startColor[0] - this.endColor[0]) / iter);
+            this.stepChengOfColor[1] = ((this.startColor[1] - this.endColor[1]) / iter);
+            this.stepChengOfColor[2] = ((this.startColor[2] - this.endColor[2]) / iter);
         }
-         /**
-        * Set color  and display him.
-        */
         this.changeColor = function()
         {
-            this.arrayColorToChanje[0] = (this.startColor[0] - Math.floor(this.stepChengOfColor[0]*this.currentFrame));
-            this.arrayColorToChanje[1] = (this.startColor[1] - Math.floor(this.stepChengOfColor[1]*this.currentFrame));
-            this.arrayColorToChanje[2] = (this.startColor[2] - Math.floor(this.stepChengOfColor[2]*this.currentFrame));
+            this.arrayColorToChange[0] = (this.startColor[0] - Math.floor(this.stepChengOfColor[0]*this.currentFrame));
+            this.arrayColorToChange[1] = (this.startColor[1] - Math.floor(this.stepChengOfColor[1]*this.currentFrame));
+            this.arrayColorToChange[2] = (this.startColor[2] - Math.floor(this.stepChengOfColor[2]*this.currentFrame));
             this.currentFrame++;
             this.element.style.backgroundColor = 'rgb(' + this.arrayColorToChanje[0] + ',' + this.arrayColorToChanje[1] + ','+ this.arrayColorToChanje[2] + ')';
             console.log(this.currentFrame);
         }
 
-        var timer = new Timer({action:this.changeColor, iteration:this.getIteration(), interval:30}); 
+        var timer = new Timer({action:this.changeColor, countIteration:this.getIteration(), interval:30}); 
 
-        this.animation = function()
+        this.animate = function()
         {
             this.setChanjeStepOfColor();
             timer.run();
@@ -149,11 +82,19 @@
 
     }
 
+    Function.prototype.myBind =  function ( context )
+    {
+        var that = this;
+
+        return  function() {
+                that.call(context);
+            };
+    }
    window.Timer = Timer; 
     function init()
     {
         var color = new AnimateColor({element:document.getElementById('container'), startColor:[12, 123, 90], endColor:[190, 255, 10], time:1500});
-        color.animation();
+        color.animate();
     }
     window.init = init;
 }())    
