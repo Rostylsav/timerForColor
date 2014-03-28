06703ba8d3586137e01d7f9376f4d58b21e70a0e
Rostyk <pas.ros.bor@gmail.com>
2014-03-28 15:44:57 +0200

    css3 + js 1.1

--- animateByCss3.js
+++ animateByCss3.js
@@ -10,8 +10,20 @@
             document.getElementById(i+'').addEventListener("click", animate);   
         }
     }
-    /**
-    * @param {event} e. set stayle class abd create some html element.
+
+    /** set stayle class abd create some html element
+    * @param {number} index. 
+    * @param {html element} container. 
+    */
+    function createHtmlElement(index, container)
+    {
+        var div = document.createElement('div');
+            div.id = index;
+            div.className = 'animation' + index;
+            container.appendChild(div);
+    }
+    /** set stayle class abd create some html element
+    * @param {event} e. .
     */
     function animate(e)
     {
@@ -23,38 +35,10 @@
 
             var container = document.getElementById('display');
             container.innerHTML = '';
-
-            var div1 = document.createElement('div');
-            div1.id = 1;
-            div1.className = 'animation' + 4;
-
-            var div2 = document.createElement('div');
-            div2.id = 2;
-            div2.className = 'animation' + 5;
-
-            var div3 = document.createElement('div');
-            div3.id = 3;
-            div3.className = 'animation' + 6;
-
-            var div4 = document.createElement('div');
-            div4.id = 4;
-            div4.className = 'animation' + 7;
-
-            var div5 = document.createElement('div');
-            div5.id = 5;
-            div5.className = 'animation' + 8;
-
-            var div6 = document.createElement('div');
-            div6.id = 6;
-            div6.className = 'animation' + 9;
-
-            container.appendChild(div1);
-            container.appendChild(div2);
-            container.appendChild(div3);
-            container.appendChild(div4);
-            container.appendChild(div5);
-            container.appendChild(div6);
-
+            for(var i = 4; i <= 9; i++)
+            {
+                createHtmlElement(i, container);
+            }
         }
         else
         {
@@ -62,9 +46,7 @@
             {
                 var container = document.getElementById('display');
                 container.innerHTML = '';
-                var div = document.createElement('div')
-                div.className = 'animation' + button.id;
-                container.appendChild(div);
+                createHtmlElement(button.id, container);
             } 
         }
         
